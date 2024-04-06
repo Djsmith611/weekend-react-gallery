0406efe2c0ade5e96e7a3dc47b8e9ba22ff3074b
David Smith <djsmith611@me.com>
2024-04-05 20:16:05 -0700

    GET greated in router

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool.js')
 
-// PUT /gallery/like/:id
-router.put('/like/:id', (req, res) => {
-  // code here
-});
 
-// GET /gallery
+// GET
 router.get('/', (req, res) => {
-  // code here
+  const queryText = `
+    SELECT * FROM "gallery"
+    ORDER BY "id";
+  `;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.status(200).send(result.rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
+// POST
+router.post('/', (req,res) => {
+
+})
+
+// PUT 
+router.put('/like/:id', (req, res) => {
+  
+});
+
+// DELETE
+router.delete('/like/:id', (req, res) => {
+  
+});
 module.exports = router;
