fb2b4bf974b8ee4a37dfcb656053abcb1e238747
David Smith <djsmith611@me.com>
2024-04-05 20:33:59 -0700

    cleaned up App file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,13 @@
 import GalleryList from "../GalleryList/GalleryList.jsx";
 import Footer from "../Footer/Footer.jsx";
+import Header from "../Header/Header.jsx";
 
 function App() {
     return (
       <div>
-        <header>
-          <h1>React Gallery</h1>
-        </header>
-
-        <p>The gallery goes here!</p>
-        <img src="images/goat_small.jpg"/>
+        <Header/>
+        <GalleryList/>
+        <Footer/>
       </div>
     );
 }
