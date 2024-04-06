d3605e6d27f3e439cff6306d11063737c997e8ed
David Smith <djsmith611@me.com>
2024-04-05 20:39:55 -0700

    linked gallery item in gallerylist

---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,11 @@
+import GalleryItemDisplay from "./GalleryItemDisplay";
+
+function GalleryList() {
+    return(
+        <div className="GalleryList">
+
+        </div>
+    )
+}
+
+export default GalleryList;
