d8211805bd5128a04b23f8da2cc919f1533b2a28
David Smith <djsmith611@me.com>
2024-04-05 20:55:27 -0700

    created GET in gallery list, linked galleryitem and passed item as prop

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,11 +1,35 @@
-import GalleryItemDisplay from "./GalleryItemDisplay";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import GalleryItem from "./GalleryItem.jsx";
 
 function GalleryList() {
+    const [gallery, setGallery] = useState([]);
+
+    const loadGallery = () => {
+        axios
+            .get("api/gallery")
+            .then((response) => {
+                setGallery(response.data);
+            })
+            .catch((err) => {
+                console.error("ERROR in GET CLIENT",err);
+                alert("Something went wrong!");
+            });
+    }
+
+    useEffect(() => {
+        loadGallery();
+    }, [])
+
     return(
         <div className="GalleryList">
-
+            {
+                gallery.map((item) => (
+                    <GalleryItem item={item}/>
+                ))
+            }
         </div>
-    )
+    );
 }
 
 export default GalleryList;
