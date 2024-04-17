086c98bd083c28d9f00b39163c3e8c69a2f1d331
David Smith <djsmith611@me.com>
2024-04-16 20:09:33 -0700

    added comments to GalleryItemDescription

--- src/components/GalleryList/GalleryItemDescription.jsx
+++ src/components/GalleryList/GalleryItemDescription.jsx
@@ -1,10 +1,16 @@
+/* IMPORT STATEMENT */
 import { CardContent, Typography } from "@mui/material";
 
+/**
+ * "footer" for Card displays
+ * @param {*} item prop passed from GalleryItem component
+ * @returns like button and like count for given item
+ */
 function GalleryItemDescription({ item }) {
   return (
-    <CardContent>
-      <Typography variant="body2" color="text.secondary">
-        {item.description}
+    <CardContent> {/* MUI CardContent component used to display content within Card component */}
+      <Typography variant="body2" color="text.secondary"> {/* MUI Typography component functions similarly to <p></p> */}
+        {item.description} {/* item description stored in item object */}
       </Typography>
     </CardContent>
   );
