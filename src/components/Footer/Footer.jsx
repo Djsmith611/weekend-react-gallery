1af5d2fb032825914d92c00776ac235b840f842f
David Smith <djsmith611@me.com>
2024-04-16 19:53:42 -0700

    added comments to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
+/* IMPORT STATEMENTS */
 import { Box, Typography } from "@mui/material";
 
+/**
+ * Footer content to be displayed in App
+ * @returns Footer element
+ */
 function Footer() {
   return (
-    <Box sx={{
+    <Box sx={{ // MUI Box for styling
         padding:1,
         textAlign:"center",
     }}>
-      <Typography variant="h5">David Smith</Typography>
+      <Typography variant="h5">David Smith</Typography> {/* MUI Typography is similar to HTML <p></p> */}
     </Box>
   );
 }
