35687fb17362c7c8e5c68c6bacab433276033f71
David Smith <djsmith611@me.com>
2024-04-16 20:37:58 -0700

    added comments to GalleryItemLikes

--- src/components/GalleryList/GalleryItemLikes.jsx
+++ src/components/GalleryList/GalleryItemLikes.jsx
@@ -10,31 +10,32 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
  */
 function GalleryItemLikes({ item, loadGallery }) {
   /**
-   * 
+   * Adds to like count of given item
    * @param {*} id for item
    */
   const likeItem = () => {
+    /* Axios call to submit a PUT request */
     axios
       .put(`api/gallery/like/${item.id}`)
       .then((res) => {
-        loadGallery();
+        loadGallery(); // Reload gallery images to display new like count
       })
-      .catch((err) => {
+      .catch((err) => { // Error handling
         console.error("ERROR in PUT CLIENT", err);
         alert("Something bad happened.");
       });
   };
 
   return (
-    <CardActions sx={{ marginTop: 3 }}>
-      <IconButton
+    <CardActions sx={{ marginTop: 3 }}> {/* MUI CardActions component holds buttons on Card component */}
+      <IconButton // MUI IconButton component allows an icon to function as a button
         onClick={() => {
-          likeItem();
+          likeItem(); // Adds to item like count
         }}
       >
-        <FavoriteIcon data-testid="like" />
+        <FavoriteIcon data-testid="like" /> {/* data-testid is used for automated testing, use npm test and try it out! */}
       </IconButton>
-      <Chip label={item.likes} />
+      <Chip label={item.likes} /> {/* MUI Chip used for styling of like count */}
     </CardActions>
   );
 }
