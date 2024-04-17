8064127a3a64661f8be0ac8894c5d0beb33c2fdc
David Smith <djsmith611@me.com>
2024-04-16 20:49:51 -0700

    added comments to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
+/* IMPORT STATEMENTS */
 import { Box, IconButton, Stack, Typography } from "@mui/material";
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 
-
-
+/**
+ * Header component for App
+ * @param {*} toggleEdit passed as prop from App
+ * @returns Header text and a button to add a photo/description
+ */
 function Header({ toggleEdit }) {
   return (
-    <Box
-      sx={{
+    <Box // MUI Box used for styling
+      sx={{ // Box styles
         padding: 0,
         paddingTop: 2,
         paddingBottom: 4,
@@ -15,18 +19,18 @@ function Header({ toggleEdit }) {
         color:"white",
       }}
     >
-      <Stack direction="row" spacing={125} >
-        <Typography
+      <Stack direction="row" spacing={125} > {/* MUI Stack used to format text and button */}
+        <Typography  // MUI Typography functions as n HTML p element
         variant="h1"
         sx={{
           textAlign: "center",
           marginBottom: 0,
         }}
-        gutterBottom
+        gutterBottom // Adds bottom margin to element
         >
         David's Gallery
         </Typography>
-        <IconButton onClick={() => {toggleEdit()}}>
+        <IconButton onClick={() => {toggleEdit()}}> {/* Allows icon to function ad a button will trigger overlay and form to display */}
           <AddAPhotoIcon sx={{color:"white"}} />
         </IconButton>
       </Stack>
