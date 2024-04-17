46fd0bc15bbb148336f356c082c76f22960c599d
David Smith <djsmith611@me.com>
2024-04-16 20:26:07 -0700

    added comments to GalleryItemHeader

--- src/components/GalleryList/GalleryItemHeader.jsx
+++ src/components/GalleryList/GalleryItemHeader.jsx
@@ -1,59 +1,76 @@
+/* IMPORT STATEMENTS */
 import { CardHeader, IconButton, Menu, MenuItem, Divider } from "@mui/material";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import MoreVertIcon from "@mui/icons-material/MoreVert"; // Vertical dots icon
 import { useState } from "react";
 import axios from "axios";
 
+/**
+ * "header" for GalleryItem
+ * @param {*} props passed from GalleryItem 
+ * @returns Image title and an options button to render the delete button
+ */
 function GalleryItemHeader({ item, loadGallery }) {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+  const [anchorEl, setAnchorEl] = useState(null); // Anchor Element for the Menu
+  const open = Boolean(anchorEl); // Boolean for if Menu is open
+  
+  /**
+   * Handles click event for MoreVertIcon/IconButton
+   */ 
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+    setAnchorEl(event.currentTarget); // Setting Menu anchor to area clicked
   };
+
+  /**
+   * Handles close event for Menu
+   */
   const handleClose = () => {
-    setAnchorEl(null);
+    setAnchorEl(null); // Removes set Menu anchor
   };
+
+  /**
+   * Handles delete button click
+   */
   const handleDelete = () => {
-    
+    /* Axios call to submit a DELETE request to the server */
     axios
       .delete(`/api/gallery/${item.id}`)
       .then((res) => {
-        loadGallery();
-        setAnchorEl(null);
+        loadGallery(); // Reloads gallery images
+        setAnchorEl(null); // Removes set Menu anchor
       })
-      .catch((err) => {
+      .catch((err) => { // Error message
         console.error(err);
       });
   }
   return (
-    <>
-    <CardHeader
-      title={item.title}
-      action={
-        <IconButton
-        id="icon-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
+    <> {/* empty element to satisfy jsx requirements while maintaining styling */}
+      <CardHeader // MUI CardHeader component contains header info for Card component
+        title={item.title} // Will display image title
+        action={ // Actions for header
+          <IconButton
+          id="icon-button" // id established for popup functionality
+          aria-controls={open ? 'basic-menu' : undefined} // Indicates Menu is controlled by the IconButton
+          aria-haspopup="true" // Indicates that a popup(Menu) is availible
+          aria-expanded={open ? 'true' : undefined}// Indicates whether the Menu is currently expanded or collapsed.
+          onClick={handleClick}
+          >
+            <MoreVertIcon />
+          </IconButton>
+        }
+      />
+      <Menu // MUI Menu component displays a popup menu on the item
+        id="basic-menu" // id established for popup functionality
+        anchorEl={anchorEl} // Anchor Element
+        open={open} // Menu open boolean
+        onClose={handleClose} // Calls when Menu is closed
+        MenuListProps={{ // Props passed to Menu
+          'aria-labelledby': 'icon-button',
+        }}
         >
-          <MoreVertIcon />
-        </IconButton>
-        
-      }
-    />
-    <Menu
-      id="basic-menu"
-      anchorEl={anchorEl}
-      open={open}
-      onClose={handleClose}
-      MenuListProps={{
-        'aria-labelledby': 'icon-button',
-      }}
-      >
-      <MenuItem onClick={handleDelete}>Delete</MenuItem>
-      <Divider sx={{ my: 0.5 }} />
-      <MenuItem onClick={handleClose}>Close</MenuItem>
-    </Menu>
+        <MenuItem onClick={handleDelete}>Delete</MenuItem> {/* Delete button */}
+        <Divider sx={{ my: 0.5 }} /> {/* Divider for styling and UI improvement */}
+        <MenuItem onClick={handleClose}>Close</MenuItem> {/* Close button to exit Menu */}
+      </Menu>
     </ >
   );
 }
