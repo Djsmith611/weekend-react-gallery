60521fbef40ec6fb4ce2c2ef28165644d7f35561
David Smith <djsmith611@me.com>
2024-04-16 20:45:09 -0700

    added comments to GalleryList

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,30 +1,37 @@
+/* IMPORT STATEMENTS */
 import { useEffect } from "react";
 import {Box} from '@mui/material';
 import GalleryItem from "./GalleryItem.jsx";
 
-
+/**
+ * A list of all gallery items
+ * @param {*} props passed from App 
+ * @returns a rendered list of all gallery images/descriptions contained in cards
+ */
 function GalleryList({loadGallery, gallery}) {
-
+    /**
+     * Loads gallery data on page load
+     */
     useEffect(() => {
         loadGallery();
     }, [])
 
     return(
-        <Box 
-            data-testid="galleryList"
-            my={4}
-            flexWrap="wrap"
+        <Box // MUI Box component used for styling
+            data-testid="galleryList" // data-testid used for automated testing, try npm test to run the sest suite!
+            my={4} // Margin set on y-axis
+            flexWrap="wrap" // Allows wrapping
             display="flex"
-            justifyContent="center"
-            alignItems="center"
-            gap={4}
-            p={2}
+            justifyContent="center" // aligns items horixontally
+            alignItems="center" // aligns items vertically
+            gap={4} // gap between items
+            p={2} // padding
             backgroundColor="#82b1ff"
             margin={0}
             >
             {
-                gallery.map((item) => (
-                    <GalleryItem key={item.id} item={item} loadGallery={loadGallery} />
+                gallery.map((item) => ( // Mapping over gallery array
+                    <GalleryItem key={item.id} item={item} loadGallery={loadGallery} /> // GalleryItem created for each item
                 ))
             }
         </Box>
